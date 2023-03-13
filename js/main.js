7bc03efefed140873919fa7b0b4fdaee9fd0b3d5
Robert <robharmony@hotmail.com>
2023-03-13 16:00:17 -0400

    category search fixed

--- js/main.js
+++ js/main.js
@@ -11,11 +11,10 @@ const modalOpen = "[data-open]";
 const modalClose = "[data-close]";
 const isVisible = "is-visible";
 
-const dataFilter='[data-filter]';
-const portfolioData= '[data-item]';
-
+const dataFilter = "[data-filter]";
+// const portfolioData = "[data-item]";
+const portfolioData = "portfolioCard";
 const root = document.documentElement;
-
 
 // Theme
 
@@ -25,14 +24,80 @@ const currentTheme = localStorage.getItem(theme);
 
 // Portfolio
 
-const filterLink= document.querySelectorAll(dataFilter);
-const portfolioItems = document.querySelectorAll(portfolioData);
-const searchBox = document.querySelector('#search');
-
+const filterLink = document.querySelectorAll(dataFilter);
+//  const portfolioItems = document.querySelectorAll(portfolioData);
+ const portfolioItems= document.getElementsByClassName(portfolioData);
+const searchBox = document.querySelector("#search");
 
 //Modal
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
+
+
+//generate cards
+const portfolioCard = document.getElementsByClassName("portfolio-card");
+
+
+
+const generatePortfolioCard = (dataItemName, picture, category, product ) => {
+ 
+ 
+  const cardDiv = document.createElement("div");
+
+  cardDiv.setAttribute("class", "portfolio-card");
+  cardDiv.setAttribute("data-item", `${dataItemName}`);
+  const grid = document.getElementsByClassName("portfolio-grid");
+  grid[0].appendChild(cardDiv);
+  // const portfolioCard = document.getElementsByClassName("portfolio-card");
+  const cardBodyDiv = document.createElement("div");
+  cardBodyDiv.setAttribute("class", "card-body");
+  portfolioCard[portfolioCard.length-1].appendChild(cardBodyDiv);
+  const cardBody = document.getElementsByClassName("card-body");
+  const img = document.createElement("img");
+  cardBody[cardBody.length-1].appendChild(img);
+  img.setAttribute("src", `${picture}`);
+  img.setAttribute("alt", "portfolio icon");
+
+  const anchorTag = document.createElement("a");
+  cardBody[cardBody.length-1].appendChild(anchorTag);
+  anchorTag.setAttribute("href", "#");
+  anchorTag.setAttribute("class", "card-popup-box");
+  const popUp = document.getElementsByClassName("card-popup-box");
+  const popUpDiv = document.createElement("div");
+  const header3 = document.createElement("h3");
+  popUp[popUp.length-1].appendChild(popUpDiv);
+  let textNode = document.createTextNode(`${category}`);
+  popUpDiv.appendChild(textNode);
+  popUp[popUp.length-1].appendChild(header3);
+  textNode = document.createTextNode(`${product}`);
+  header3.appendChild(textNode);
+  
+
+};
+generatePortfolioCard("web","assets/images/portfolio-1.jpg", "Web Development","Food Website" )
+generatePortfolioCard("web","assets/images/portfolio-2.jpg", "Web Development","Skate Website" )
+generatePortfolioCard("web","assets/images/portfolio-3.jpg", "Web Development","Eating Website" )
+generatePortfolioCard("web","assets/images/portfolio-4.jpg", "Web Development","Cool Website" )
+generatePortfolioCard("app","assets/images/portfolio-5.jpg", "Web Development","Game App" )
+generatePortfolioCard("app","assets/images/portfolio-6.jpg", "Web Development","Gambling App"  )
+generatePortfolioCard("app","assets/images/portfolio-7.jpg", "Web Development","Money Website" )
+generatePortfolioCard("ui","assets/images/portfolio-8.jpg", "Web Development","Fantastic Design" )
+
+
+
+searchBox.addEventListener("keyup", (e) => {
+  const searchInput = e.target.value.toLowerCase().trim();
+  console.log(searchInput);
+  for(let i =0; i < portfolioCard.length; i++){ 
+    if (portfolioCard[i].dataset.item.includes(searchInput)) {
+      portfolioCard[i].style.display = "block";
+    } else {
+      portfolioCard[i].style.display = "none";
+    }
+  };
+});
+
+
 
 const setActive = (element, selector) => {
   if (document.querySelector(`${selector}.${active}`) !== null) {
@@ -80,37 +145,38 @@ for (const elm of switcher) {
   });
 }
 
-const generatePortfolioCard = (picture, category, product,dataItem) =>{
 
-}
+// searchBox.addEventListener("keyup", (e) => {
+//   const searchInput = e.target.value.toLowerCase().trim();
+//   console.log(searchInput);
+//   portfolioItems.forEach((card) => {
+//     if (card.dataset.item.includes(searchInput)) {
+//       card.style.display = "block";
+//     } else {
+//       card.style.display = "none";
+//     }
+//   });
+// });
 
-
-searchBox.addEventListener('keyup', (e)=>{
-const searchInput = e.target.value.toLowerCase().trim();
-portfolioItems.forEach((card)=>{
-  if(card.dataset.item.includes(searchInput)){
-    card.style.display='block';
-  }else{
-    card.style.display='none';
-  }
-})
-
-})
-
-for(const link of filterLink){
-  link.addEventListener('click', function(){
-    setActive(link, '.filter-link')
+for (const link of filterLink) {
+  link.addEventListener("click", function () {
+    setActive(link, ".filter-link");
     const filter = this.dataset.filter;
-    portfolioItems.forEach((card)=>{
-if(filter==='all'){
-  card.style.display = 'block';
-}else if(card.dataset.item ===filter){
-  card.style.display= 'block';
-}else{
-  card.style.display = 'none';
-}
-    })
-  })
+    console.log(filter)
+   
+    console.log(portfolioItems);
+    for(let i =0; i < portfolioCard.length; i++) {
+      
+      if (filter === "all") {
+        portfolioCard[i].style.display = "block";
+      } else if (portfolioCard[i].dataset.item === filter) {
+        portfolioCard[i].style.display = "block";
+      } else {
+        portfolioCard[i].style.display = "none";
+      }
+    };
+    console.log(portfolioCard);
+  });
 }
 
 //Full site Modal "open Buttons"
@@ -126,36 +192,35 @@ for (const elem of closeModal) {
     this.parentElement.parentElement.classList.remove(isVisible);
   });
 }
-const grid = document.getElementsByClassName("portfolio-grid");
+// const grid = document.getElementsByClassName("portfolio-grid");
 
+// const cardDiv =  document.createElement("div");
 
+// cardDiv.setAttribute("class", "portfolio-card")
+// cardDiv.setAttribute("data-item", "web")
 
-const cardDiv =  document.createElement("div");
+// grid[0].appendChild(cardDiv);
+// const portfolioCard=document.getElementsByClassName("portfolio-card");
+// const cardBodyDiv =  document.createElement("div")
+// cardBodyDiv.setAttribute("class", "card-body");
+// portfolioCard[0].appendChild(cardBodyDiv);
+// const cardBody = document.getElementsByClassName("card-body");
+// const img = document.createElement("img");
+// cardBody[0].appendChild(img);
+// img.setAttribute("src","assets/images/portfolio-1.jpg")
+// img.setAttribute("alt","portfolio icon")
 
-cardDiv.setAttribute("class", "portfolio-card")
-cardDiv.setAttribute("data-item", "web")
+// const anchorTag = document.createElement("a");
+// cardBody[0].appendChild(anchorTag);
+// anchorTag.setAttribute("href","#")
+// anchorTag.setAttribute("class","card-popup-box")
+// const popUp = document.getElementsByClassName("card-popup-box");
+// const popUpDiv = document.createElement("div");
+// const header3 = document.createElement("h3");
+// popUp[0].appendChild(popUpDiv);
+// let textNode= document.createTextNode("Web development")
+// popUpDiv.appendChild(textNode);
+// popUp[0].appendChild(header3);
+// textNode= document.createTextNode("Food Website")
+// header3.appendChild(textNode);
 
-grid[0].appendChild(cardDiv);
-const porfolioCard=document.getElementsByClassName("portfolio-card");
-const cardBodyDiv =  document.createElement("div")
-cardBodyDiv.setAttribute("class", "card-body");
-porfolioCard[0].appendChild(cardBodyDiv);
-const cardBody = document.getElementsByClassName("card-body");
-const img = document.createElement("img");
-cardBody[0].appendChild(img);
-img.setAttribute("src","assets/images/portfolio-1.jpg")
-img.setAttribute("alt","portfolio icon")
-
-const anchorTag = document.createElement("a");
-cardBody[0].appendChild(anchorTag);
-anchorTag.setAttribute("href","#")
-anchorTag.setAttribute("class","card-popup-box")
-const popUp = document.getElementsByClassName("card-popup-box");
-const popUpDiv = document.createElement("div");
-const header3 = document.createElement("h3");
-popUp[0].appendChild(popUpDiv);
-let textNode= document.createTextNode("Web development")
-popUpDiv.appendChild(textNode);
-popUp[0].appendChild(header3);
-textNode= document.createTextNode("Food Website")
-header3.appendChild(textNode);
